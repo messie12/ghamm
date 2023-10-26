099249cec1340057a1a6401ce1d8f8d3dbc448cc
super <triphenedimbote@gmail.com>
2023-10-26 02:33:44 +0200

    apdate app.py

--- app.py
+++ app.py
@@ -205,6 +205,6 @@ def payement():
         mysql.connection.commit()
         return redirect(url_for("Data"))
  
-if __name__ =='__main__':
-   port = int(os.environ.get('PORT', 5000))
-   app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
