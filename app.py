312fde1f8ae3fe1485250d695156834fac35def1
super <triphenedimbote@gmail.com>
2023-11-02 13:02:03 +0100

    apld

--- app.py
+++ app.py
@@ -19,8 +19,6 @@ app.config['MYSQL_DATABASE_PASSWORD'] = os.environ.get('DB_PASSWORD')
 app.config['MYSQL_DATABASE_DB'] = os.environ.get('DB_NAME')
 app.config['MYSQL_DATABASE_PORT'] = 33848  
 
-mysql = MySQL()
-mysql.init_app(app)
 
 '''app=Flask(__name__)
 app.secret_key= "secret_key"
@@ -29,8 +27,6 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '' 
 app.config['MYSQL_DB'] = 'horizon'
 '''
-
-
 
 
 # Créez une instance MySQL en utilisant la configuration de votre application
