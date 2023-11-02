d339daca7d7598dfbb4584774fbdfddd3e97609d
super <triphenedimbote@gmail.com>
2023-11-02 05:51:37 +0100

    upd

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.secret_key = "secret_key"
 
 app.config['MYSQL_HOST'] = os.environ.get('DB_HOST')
 app.config['MYSQL_USER'] = os.environ.get('DB_USER')
-app.config['MYSQL_PASSWORD'] = os.environ.get('DB_PASSWORD')
+app.config['MYSQL_PASSWORD'] = int(os.environ.get('DB_PASSWORD'))
 app.config['MYSQL_DB'] = os.environ.get('DB_NAME')
 
 '''app=Flask(__name__)
