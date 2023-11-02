355fd98fe7c99d05519525054b3d64e80f9102ea
super <triphenedimbote@gmail.com>
2023-11-02 06:29:55 +0100

    ttt

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 app.secret_key = "secret_key"
 
 app.config['MYSQL_HOST'] = os.environ.get('DB_HOST')
-app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT'))
+app.config['MYSQL_PORT'] = os.environ.get('MYSQL_PORT')
 app.config['MYSQL_USER'] = os.environ.get('DB_USER')
 app.config['MYSQL_PASSWORD'] = os.environ.get('DB_PASSWORD')
 app.config['MYSQL_DB'] = os.environ.get('DB_NAME')
