d40232fcaf815ad5846785a94bb9709ea92e6c69
super <triphenedimbote@gmail.com>
2023-11-04 15:55:38 +0100

    upld

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
-from flask_mysqldb import MySQL
+from flask import Flask, render_template,request, redirect, url_for,flash, jsonify
+from flask_mysqldb import MySQL 
 
-import os
+app=Flask(__name__)
+app.secret_key= "secret_key"
 
-from flask import Flask
-
- 
-app = Flask(__name__)
-
-app.secret_key = "secret_key"
 app.config['MYSQL_HOST'] = 'f173a5bc-69f8-488f-b6a8-f931274e57f3.ghamm-servi-5741.mysql.a.osc-fr1.scalingo-dbs.com'
 app.config['MYSQL_PORT'] = 33848
 app.config['MYSQL_USER'] = 'ghamm_servi_5741'
@@ -16,68 +11,74 @@ app.config['MYSQL_PASSWORD'] = 'OLlaqQ9XfUuSitHRUKL6'
 app.config['MYSQL_DB'] = 'ghamm_servi_5741'
 
 
-"""app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '' 
-app.config['MYSQL_DB'] = 'horizon'"""
-
-
-
-
-# Créez une instance MySQL en utilisant la configuration de votre application
 mysql = MySQL(app)
 
-# Exemple de route pour recevoir des données via POST
-@app.route('/T1', methods=['POST'])
-def receive_data():
+@app.route('/T1', methods=['POST','POST'])
+def receive_data(): 
+    print('la fonction est deja appelée')
     data = request.get_json()
-    print("Received data:", data)
-    donnees = data.values()
-    _donnees = tuple(donnees)
-    print("tuple de données ------------------------", _donnees)
-    try:
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO EnregistrementMoto (Nom_chauffeur, Proprietaire, Num_moteur, N_chasie, Marque, Couleur, Secteur, Tel_prop, Date) VALUES (%s, %s, %s, %s, %s, %s, %s , %s,  STR_TO_DATE('04/11/2023 04:42', '%d/%m/%Y %H:%i')))", _donnees)
-        mysql.connection.commit()
-        cur.close()
+    print("Received data:", data) 
+    donnees=data.values()
+    _donnees=tuple(donnees)
+    print("tuple de donner------------------------",_donnees)
+    cur = mysql.connection.cursor()	
+    cur.execute("INSERT INTO epargne_motocyclette (Nom_chauffeur, Proprietaire, Num_moteur, N_chasie, Marque, Couleur, Parking ) VALUES( %s, %s, %s, %s, %s, %s, %s)",_donnees)
+    mysql.connection.commit()
+    # Traitez les données reçues ici selon vos besoins
+    print("okokokokokok")
+    return jsonify({"message": " succés"})
 
-        return jsonify({"message": "Succès"})
-    except Exception as e:
-        print(f'mysql: {e}')
-        return jsonify({"message": "Erreur lors de l'insertion dans la base de données"})
-    
-# Exemple de route pour recevoir des données via POST
-@app.route('/matricule', methods=['POST'])
-def receive_dat():
-    print('La fonction est déjà appelée')
-    data = request.get_json()
-    print("Received data:", data)
-    donnees = data.values()
-    _donnees = tuple(donnees)
-    try:
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM EnregistrementMoto WHERE N_chasie=%s", _donnees)
-        results = cur.fetchone()
-        results = list(results) if results else None
-        mysql.connection.commit()
-        cur.close()
-        return jsonify({'donnees': results})
+
+
+
+
+
+
+@app.route('/matricule', methods=['POST','POST'])
+def receive_dat(): 
+    print('la fonction est deja appelée')
+    data = request.get_json() 
+    print("Received data:", data) 
+    donnees=data.values()
+    _donnees=tuple(donnees)
+    print("tuple de donner------------------------",_donnees)
+    try:  
+            cur = mysql.connection.cursor()
+            cur.execute( "SELECT * FROM epargne_motocyclette WHERE Num_moteur=%s", _donnees)
+            results = cur.fetchone() 
+            results=list(results) 
+            mysql.connection.commit()
+   
+            if results==None:
+                print(results)
+                return jsonify({'donnees': results})
+            else:
+                print(results)
+                return jsonify({'donnees': results})
     except:
-        print("Les formats des données sont refusé")
+        print("le format de donné refusé")
 
 @app.route('/get_donnees', methods=['GET'])
 def get_donnees():
     matricules = request.args.get('matricule')
-    print('ici nous sommes dans get', matricules)
+    print('ici mous somme dans get', matricules)
     tuples = (matricules, )
     cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM EnregistrementMoto WHERE Num_moteur=%s", tuples)
+    cur.execute("SELECT * FROM epargne_motocyclette WHERE Num_moteur=%s", tuples)
     donnees = cur.fetchone()
     mysql.connection.commit()
-    cur.close()
-    donnees_list = list(donnees) if donnees else []
+    
+    if donnees is None:
+        donnees_list = []
+    else:
+        donnees_list = list(donnees)
+    
     print(donnees_list)
     return jsonify(donnees_list)
+
+
+ 
+
 
 
 @app.route('/') 
@@ -85,36 +86,60 @@ def index_acceuil():
     return  render_template("acceuilx.html") 
 
 
-@app.route("/traitement_epargne", methods =[ 'POST'])
+@app.route("/traitement_epargne", methods =[ 'POST','GET'])
 def traitement_epargne():
      
+    if request.method == "POST":
         donne_form_log_epgne = request.form
         nom=donne_form_log_epgne.get('identifiant')
         passe=donne_form_log_epgne.get('motdepasse')
         donner=(nom,passe)
-        
         try:
             cur = mysql.connection.cursor()	
-            print(donner)
-            cur.execute( "SELECT * FROM login WHERE Nom_utilisateur=%s and Mot_de_passe =%s", donner)
+            cur.execute( "SELECT * FROM login WHERE Nom_utilisateur= %s AND Mot_de_passe = %s", donner)
             results = cur.fetchone() 
-            print("okkookkook",results)
-
-            if results!=None:
-                cur = mysql.connection.cursor()
-                cur.execute("SELECT * FROM EnregistrementMoto  ")
-                data = cur.fetchall()
-
-                return render_template("shows_data.html",id_utilisateur=results[1] ,payement_terminaux=data,)
-            else:
-                return '<h1>Erreur mot de passe<h1>'
+            print(results)
+            
         except Exception as e:
-            print(f"Erreur MySQL: {e}")
-            return '<h1>Erreur de liaison avec la base de donner<h1>'
-     
-       
-          
+            print(f"mysql:{e}")
+            return 'erreur de connexion sur le serveur'
+            print(f"mysql:{e}")
+        if results == None:
+            return redirect(url_for("traitement_epargne"))
+        else:
+            cur = mysql.connection.cursor()
+            cur.execute("SELECT * EnregistrementMoto  ")
+            data = cur.fetchall()
+
+
+            cur.execute("SELECT COUNT(*) FROM  epargne_motocyclette" )
+            nobreDenregistrment=cur.fetchone()[0]*500
+            cur.close()
+            
+            return render_template("shows_data.html",id_utilisateur=results[0],payement_terminaux=data, nbr_enregis=nobreDenregistrment)
+    else:
+      return redirect(url_for("traitement_epargne"))
   
+@app.route("/upload", methods =['POST'])
+def epargn_upload():
+    if request.method == "POST":
+        donne_form_base = request.form
+        print(donne_form_base)
+        return"les donnees sont envoyer avec succes "
+    else:
+        return redirect(url_for("index_acceuil"))
+       
+
+
+@app.route('/donnees', methods =["GET"] )
+def Data():
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * from epargne_motocyclette  ")
+        data = cur.fetchall()
+        cur.close()
+        return render_template("incude_tabl.html", payement_terminaux=data )
+    
+
 
 
 
@@ -132,12 +157,80 @@ def login_epargne():
           
 
 
+@app.route('/insert', methods = ['POST'])
+def insert():
+
+    if request.method == "POST":
+        
+        nom = request.form['nom']
+        matric = request.form['matric']
+        tel = request.form['tel']
+        cur = mysql.connection.cursor()
+        
+        mysql.connection.commit()
+        flash("les données sont inseré avec succes")
+        
+
+@app.route('/delete/<string:id_data>', methods = ['GET'])
+def delete(id_data):
+ try:
+    flash("Record Has Been Deleted Successfully")
+    cur = mysql.connection.cursor()
+    cur.execute("DELETE FROM compt_client WHERE id=%s", (id_data,))
+    mysql.connection.commit()
+    return redirect(url_for('Data'))
+ except: 
+    return"la supression de cette compte entrainera les pertes de donnés de payement" 
+    
+    
+@app.route('/update',methods=['POST','GET'])
+def update():
+   if request.method == 'POST':
+        id_data= request.form['id']
+        nom = request.form['nom']
+        matric = request.form['matric']
+        tel = request.form['tel']
+        print(request.form)
+        num_compt = request.form['num_compt'] 
+        cur = mysql.connection.cursor()
+        cur.execute(""" UPDATE compt_client
+               SET Nom = %s, Matricule = %s, Telephone = %s 
+               WHERE id=%s """,( nom, matric, tel, num_compt, id_data,))
+        flash("Data Updated Successfully")
+        mysql.connection.commit()   
+        return redirect(url_for('Data'))
+
+
+@app.route('/detail/<string:id_data>')
+def detail_clent(id_data):
+        cur = mysql.connection.cursor()
+        cur.execute("""SELECT p.* FROM paiements p JOIN compt_client c ON c.id = %s""" % (id_data))
+
+        donne = cur.fetchall()
+        cur.close()
+        global shared_variable
+        don_client =list(donne)
+        print(donne)          
+        return render_template('detail.html',details=donne)
+
+@app.route('/payement', methods = ['POST',"GET"])
+def payement():
+     if request.method == "POST":
+        num_carnet = request.form['num_carnet']
+        payement = request.form['payement']
+        id_datas= request.form ['id']
+        print(num_carnet,payement)
+        print("voici",id_datas)
+        flash("Record Has Been Deleted Successfully")
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO paiements(Numerocarnet, Montantpayer, client_id) VALUES (%s, %s, %s)", (num_carnet, payement, id_datas))
+        mysql.connection.commit()
+        return redirect(url_for("Data"))
  
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+                 
 
 
-if __name__ == "gunicorn.app.wsgiapp":
-    from app import app  
-    application = app
+
+if __name__ =='__main__':
+   
+   app.run(host='0.0.0.0', port=5000)
